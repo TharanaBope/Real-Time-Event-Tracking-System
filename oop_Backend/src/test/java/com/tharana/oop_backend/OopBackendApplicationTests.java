b739a07c3eb8e84e3a19db683cbe9d5173489046
Tharana Bopearachchi <tharanabope30@gmail.com>
2024-12-09 13:19:17 +0530

    CLI

---
+++ oop_Backend/src/test/java/com/tharana/oop_backend/OopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tharana.oop_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
